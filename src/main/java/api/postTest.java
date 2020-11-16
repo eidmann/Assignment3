40e9747652c304789479b74f8cea76be852199cb
Johan Eidmann <johan.eidmann1995@hotmail.com>
2020-11-16 19:47:15 +0100

    second commit

---
+++ src/main/java/api/postTest.java
@@ -0,0 +1,17 @@
+package main.java.api;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path ("test")
+public class postTest {
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response test() {
+		return Response.ok("Hej").build();
+	}
+}
